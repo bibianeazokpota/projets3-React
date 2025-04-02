32130040050d263e47814a49a109b32a0b9f8936
unknown <djiwabssiti7@gmail.com>
2025-04-02 15:52:26 +0100

    footer

---
+++ projet3/src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='div'>
+        <footer className='foot'>
+        <div>
+            <div><a href="Accueil">Accueil</a></div>
+        </div> 
+        <div>
+            <div><a href="Profil">Profil</a></div>
+        </div>
+        <div>
+            <div><a href="Form">Form</a></div>
+        </div>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
