6974f6e309de888745ebf352e17a16245d594f47
unknown <djiwabssiti7@gmail.com>
2025-04-02 22:11:21 +0100

    Footer modifier

--- projet3/src/Components/Footer.jsx
+++ projet3/src/Components/Footer.jsx
@@ -5,15 +5,17 @@ function Footer() {
   return (
     <div className='div'>
         <footer className='foot'>
-        <div>
-            <div><a href="Accueil">Accueil</a></div>
-        </div> 
-        <div>
-            <div><a href="Profil">Profil</a></div>
-        </div>
-        <div>
-            <div><a href="Form">Form</a></div>
-        </div>
+          <div id='divContener'>
+            <div>
+              <a href="Accueil">Accueil</a>
+            </div> 
+            <div>
+              <a href="Profil">Profil</a>
+            </div>
+            <div>
+              <a href="Form">Form</a>
+            </div>
+          </div>
         </footer>
     </div>
   )
