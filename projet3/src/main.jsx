2c21f37ee0a7fa4d648d9c0c3cbbae2c56f504e3
unknown <djiwabssiti7@gmail.com>
2025-04-03 14:27:18 +0100

    ¨[UPDATE] finalize foter

--- projet3/src/main.jsx
+++ projet3/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Components/Footer.css'
-import App from './App.jsx'
 import Footer from './Components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
