959c53e4ac1a2f85a77874976a3ff8079db29223
bibianeazokpota <bayibibianeazokpota@gmail.com>
2025-04-03 18:27:44 +0200

    Ajout des modifications sur la branche footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import NavBar from './components/NavBar.jsx' 
-import './components/NavBar.css'
+import NavBar from './Components/NavBar.jsx' 
+import './Components/NavBar.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
