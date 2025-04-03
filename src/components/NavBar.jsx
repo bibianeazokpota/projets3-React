67a5b6c427ed1c01bbb5e4b3e371db4dfa242377
unknown <djiwabssiti7@gmail.com>
2025-04-03 16:02:38 +0100

    [ADD]

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <div>
+            <a href="accueil">accueil</a>
+        </div>
+        <div>
+            <a href="profil">profil</a>
+        </div>
+        <div>
+            <a href="form">form</a>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
